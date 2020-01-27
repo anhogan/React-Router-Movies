814198c7b3d679cec9fcfb00a999b3107136a4a5
Amanda Hogan <ahogan@teamworks.com>
2020-01-27 12:03:00 -0500

    Import MovieCard component to MovieList component

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -34,22 +34,6 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      {/* <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div> */}
       <MovieCard title={movie.title} director={movie.director} metascore={movie.metascore} stars={movie.stars} id={movie.id}/>
       <div className="save-button">Save</div>
     </div>
